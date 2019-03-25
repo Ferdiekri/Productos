eee0c052cd35446051edd18b2214ca3a04f34885
hobetuz <hobetuz@ipartekaula.com>
2019-03-25 20:16:05 +0100

    creado el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
